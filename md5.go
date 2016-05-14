ae8f51066bd1b581ad96d097236aa32c518bfeaa
cexoyq <34512664@qq.com>
2016-05-14 22:11:11 +0800

    NEW

---
+++ md5.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"encoding/hex"
+)
+
+func main() {
+	h := md5.New()
+	//io.WriteString(h, "The fog is getting thicker!")
+	//io.WriteString(h, "And Leon's getting laaarger!")
+	io.WriteString(h,"cexoyq1020")
+	fmt.Println("%x", h.Sum(nil),"%s", h.Sum(nil))
+	io.WriteString(h,"cexoyq1020")
+	fmt.Println("%x", h.Sum(nil),"%s", h.Sum(nil))
+	fmt.Printf("cexoyq1020:%s\n", hex.EncodeToString(h.Sum(nil))) // 输出加密结果
+	data := []byte("These pretzels are making me thirsty.")
+	fmt.Println("%x", md5.Sum(data))
+	// md5 加密的第一种方法，也可以用
+	srcData := []byte("szq")
+	cipherText1 := md5.Sum(srcData)
+	fmt.Printf("md5 encrypto is \"szq\": %x \n", cipherText1)
+	srcData = []byte("cexoyq1020")
+	cipherText1 = md5.Sum(srcData)
+	fmt.Printf("md5 encrypto is \"cexoyq1020\": %x \n", cipherText1)
+	/*用的下面这种没问题**********/
+	h = md5.New()
+    h.Write([]byte("cexoyq1020")) // 需要加密的字符串为 cexoyq1020
+    fmt.Printf("cexoyq1020:%s\n", hex.EncodeToString(h.Sum(nil))) // 输出加密结果
+	h.Write([]byte("szq")) // 需要加密的字符串为
+    fmt.Printf("szq:%s\n", hex.EncodeToString(h.Sum(nil))) // 输出加密结果
+}
---
+++ t2.go
@@ -0,0 +1,72 @@
+//测试封装websocket，
+package main
+
+import (
+	"flag"
+	"log"
+	"net/url"
+	"net"
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", "192.168.1.112:80", "http service address")
+type Conn struct {
+	conn    net.Conn
+}
+
+func (this *Conn) Close(){
+	this.conn.Close()
+}
+func (this *Conn)Init() error{
+	var err error
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
+	log.Printf("connecting to %s", u.String())
+	this, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+		return err
+	}
+	return nil
+}
+
+func (this *Conn)SendServerStat(msg string) error {
+	var err error
+	err = this.WriteJSON(msg)
+	if err != nil {
+		log.Println("write:", err)
+		return err
+	}
+	return nil
+}
+
+func (this *Conn)ReadServerStat() error {
+	var err error
+	_, message, err := this.ReadMessage()
+	if err != nil {
+		return err
+	}
+	log.Printf("read:%s",message)
+	return nil
+}
+
+func main() {
+	var (
+		//c *websocket.Conn
+		conn Conn
+	)
+	/*u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
+	log.Printf("connecting to %s", u.String())
+	c, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()*/
+	conn.Init();
+	defer conn.Close();
+	msg := "ehehe"
+	log.Printf("write:",msg)
+	//conn=c;
+	conn.SendServerStat(msg)
+	conn.ReadServerStat()
+	return
+}
